559f2715e6bd348633a592aca0fdc208d0afcb63
liuweibo <liuweibo@haier.com>
2023-03-14 12:10:53 +0800

    fix: 修复提交msg

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ try {
   const lastSubmit = execa.shellSync(`git log -1 --date=iso ${branch}`).stdout;
   const gitInfo = execa.shellSync(`git config --list`).stdout;
   lastSubmitTime = lastSubmit.match(/\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/g)[0];
-  const lastSubmitMatch = lastSubmit.match(/(\b)+(.*)/g);
-  lastSubmitTxt = lastSubmitMatch ? lastSubmitMatch[5] : '';
+  const lastSubmitMatch = lastSubmit.match(/(\b)?(.*)/g);
+  lastSubmitTxt = lastSubmitMatch ? lastSubmitMatch[7] : '';
   publishPerson = gitInfo.match(/(?<=user.name=)(.*)/g)[0];
 } catch (error) {
   console.log('BuildTime Plugin Err', error);
 }
 const time = new Date().toLocaleString();
-const buildTime = `构建时间:${time};构建人:${publishPerson};分支:${branch};最后提交msg:${lastSubmitTxt};最后提交时间:${lastSubmitTime};最后提交commitId:${lastId};`;
+const buildTime = `构建时间:${time};构建人:${publishPerson};分支:${branch};最后提交msg:${lastSubmitTxt.trim()};最后提交时间:${lastSubmitTime};最后提交commitId:${lastId};`;
 const buildFn = () => {
   return {
     buildTime: time,
