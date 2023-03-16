fdf1ddeda16f09134826e246c681995eba4ac768
liuweibo <liuweibo@haier.com>
2023-03-16 11:38:56 +0800

    fix: 修复提交msg

--- index.js
+++ index.js
@@ -4,23 +4,38 @@ Object.defineProperty(exports, '__esModule', { value: true });
 
 var execa = require('execa');
 
-let lastId = '';
-let branch = '';
-let lastSubmitTime = '';
-let lastSubmitTxt = '';
-let publishPerson = '';
-try {
-  lastId = execa.shellSync('git rev-parse HEAD').stdout;
-  branch = execa.shellSync('git symbolic-ref --short HEAD').stdout;
-  const lastSubmit = execa.shellSync(`git log -1 --date=iso ${branch}`).stdout;
-  const gitInfo = execa.shellSync(`git config --list`).stdout;
-  lastSubmitTime = lastSubmit.match(/\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/g)[0];
-  const lastSubmitMatch = lastSubmit.match(/(\b)?(.*)/g);
-  lastSubmitTxt = lastSubmitMatch ? lastSubmitMatch[7] : '';
-  publishPerson = gitInfo.match(/(?<=user.name=)(.*)/g)[0];
-} catch (error) {
-  console.log('BuildTime Plugin Err', error);
+const defaultIdLog = 'git rev-parse HEAD';
+const defaultBranchLog = 'git symbolic-ref --short HEAD';
+function getGitCommitInfo (sign, commitId) {
+  const log = `git log --pretty=format:"${sign}" ${commitId} -1`;
+  return execa.shellSync(log).stdout;
 }
+function getGitInfo (idLog = defaultIdLog) {
+  try {
+    const lastId = execa.shellSync(idLog).stdout;
+    const branch = execa.shellSync(defaultBranchLog).stdout;
+    const lastSubmitTxt = getGitCommitInfo('%s', lastId);
+    const lastSubmitTime = getGitCommitInfo('%cd', lastId);
+    const publishPerson = getGitCommitInfo('%an', lastId);
+    return {
+      lastId,
+      branch,
+      lastSubmitTxt,
+      lastSubmitTime,
+      publishPerson,
+    };
+  } catch (error) {
+    console.log('BuildTime Plugin Err', error);
+    return {};
+  }
+}
+const {
+  lastId,
+  branch,
+  lastSubmitTxt,
+  lastSubmitTime,
+  publishPerson,
+} = getGitInfo();
 const time = new Date().toLocaleString();
 const buildTime = `构建时间:${time};构建人:${publishPerson};分支:${branch};最后提交msg:${lastSubmitTxt.trim()};最后提交时间:${lastSubmitTime};最后提交commitId:${lastId};`;
 const buildFn = () => {
